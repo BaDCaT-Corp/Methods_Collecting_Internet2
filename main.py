63f968f6fe8228b9635843ab137bf254ff20c946
Ivan <kurdinivan@gmail.com>
2022-01-07 22:58:56 +0300

    add lesson_003

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import Lesson_01
 import Lesson_02
+import Lesson_03
 
 if __name__ == '__main__':
     # Lesson_01.lesson_01()
-    
-    Lesson_02.lesson_02()
+
+    # Lesson_02.lesson_02()
+
+    Lesson_03.lesson_03()
