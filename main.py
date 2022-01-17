acdfa03275d28086f62c1ce5c3326bc829873574
Ivan <kurdinivan@gmail.com>
2022-01-17 19:26:09 +0300

    add lesson_05

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import Lesson_01
 import Lesson_02
 import Lesson_03
 import Lesson_04
+import Lesson_05
 
 if __name__ == '__main__':
     # Lesson_01.lesson_01()
@@ -10,4 +11,7 @@ if __name__ == '__main__':
 
     # Lesson_03.lesson_03()
 
-    Lesson_04.lesson_04()
+    # Lesson_04.lesson_04()
+
+    Lesson_05.lesson_05()
+
